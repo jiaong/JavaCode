25b23d25666d886f4090388729cf7f82a42d4fcb
jiaong <jiaong.yy@gmail.com>
2021-02-08 15:19:41 +0800

    2021-02-07 update

---
+++ JTest/src/String/StringReverse.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Scanner;
+
+/**
+ * StringReverse
+ * 定义一个方法，实现字符串反转
+ * 键盘录入 abc，输出结果 cba
+ *
+ * @Version : 1.0
+ * @Author : jiaong
+ * @Email : jiaong.yy@gmail.com
+ * @Date : 2021/2/8 15:09
+ */
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入一个字符串！");
+        String line = sc.nextLine();
+        String s=reverse(line);
+        System.out.println(s);
+    }
+
+    public static String reverse(String s) {
+        String ss = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            ss += s.charAt(i);
+        }
+        return ss;
+    }
+}
+
